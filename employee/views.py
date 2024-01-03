509203e32ca670aee29a4b9b63c1017e9738643a
shahariar <shahariar@shahariar.localdomain>
2024-01-03 12:06:36 +0600

    single api with all functionality

---
+++ employee/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .serializers import EmployeeSerializer
+from rest_framework import viewsets
+from .models import Employee
+from .pagination import Pagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter,OrderingFilter
+
+# Create your views here.
+
+class EmployeeApi(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    pagination_class = Pagination
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_fields = ['name']
+    search_fields = ['position']
+    ordering_fields = ['name']
+    
